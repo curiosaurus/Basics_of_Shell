9cdceaac8932ff7fc635b4937e6a89fee3ee8751
Pratik Patil <35042555+curiosaurus@users.noreply.github.com>
2019-09-10 10:51:41 +0530

    Add files via upload

---
+++ lol.sh
@@ -0,0 +1,10 @@
+echo 'enter num1'
+read num1
+echo 'enter num2'
+read num2
+if [ $num1 -gt $num2 ]
+then 
+echo 'num1 is greater '$num1
+else
+echo 'num2 is greater '$num2
+fi
---
+++ strcmppd.sh
@@ -0,0 +1,10 @@
+echo 'enter the first string'
+read str1
+echo 'enter the second string'
+read str2
+if [ $str1 = $str2 ]
+then
+	echo 'both are equal strings'
+ else
+	echo 'strings are not equal'
+fi	
